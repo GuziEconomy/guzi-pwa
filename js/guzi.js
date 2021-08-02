ff3cf25e1c762977a1384fc89f0107d20e797308
Gus <guillaume1.dubus@gmail.com>
2021-08-02 19:56:44 +0200

    WIP : Key pair creation and birthblock

--- js/guzi.js
+++ js/guzi.js
@@ -1,6 +1,73 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+    checkAccountIsValidOrCreateOne();
     updateContacts();
 });
+
+function checkAccountIsValidOrCreateOne() {
+    return localforage.getItem('guzi-blockchain').then(blockchain => {
+        if (blockchain === null) {
+            var accountModal = new bootstrap.Modal(document.getElementById('newAccountModal'));
+            accountModal.show();
+        }
+    });
+}
+
+function createAccountFromModal() {
+    const birthdate = document.getElementById("new-account-modal-birthdate").value;
+    const keyPair = await window.crypto.subtle.generateKey(
+        {
+            name: "ECDSA",
+            namedCurve: "P-256",
+            hash: "SHA-256"
+        },
+        true,
+        ["sign", "verify"]
+    );
+    const keyPair = await crypto.subtle.exportKey("jwk", keyPair2.privateKey);
+    const myPubKey = 0;
+    // Create the first block of the blockchain
+    // 1. Birthday Block :
+    let birthblock = {
+        v: 1, // Version
+        d: birthdate, // User birth date
+        ph: "C1A551CA1C0DEEA5EFEA51B1E1DEA112ED1DEA0A5150F5E11AB1E50C1A15EED5", // Previous hash : here random
+        s: myPubKey, // Signer public key, here the new one created
+        g: 0, b: 0, t: 0, // 0 guzis, 0 boxes, 0 total
+    }
+    
+    hashblock(birthblock);
+
+    //let initializationblock = {
+    //    v: 1,
+    //    d: Date.now(),
+    //    ph: hashblock(birthblock),
+    //    s: refPubKey,
+    //    g: 1, b: 1, t: 0,
+    //    tc: 2, tx: [
+    //        {v: 1, t: 0, d: Date.now(), t: myPubKey, a: 1, h: null},
+    //        {v: 1, t: 1, d: Date.now(), t: myPubKey, a: 1, h: null}
+    //    ],
+    //    ec: 0, en: []
+    //}
+    // TODO : 
+    // - Check password and confirmation are the same
+    // - make hashblock() method (or a makeBlock or somthing)
+    // - save the blockchain
+}
+
+async function hashblock(block) {
+    const packed = msgpack.encode(block);
+    const digest = await crypto.subtle.digest('SHA-256', packed);
+
+    console.log(packed);
+    console.log(digest);
+    console.log(await crypto.subtle.digest('SHA-256', msgpack.encode(msgpack.decode(packed))));
+}
+
+// TODO : use messagepack and create hashes
+function makeTx(type, target, amount) {
+    let tx = {v: 1, t: type, d: Date.now(), t: target, a: amount, h: null};
+}
 
 function updateContacts() {
     localforage.getItem('guzi-contacts').then(contacts => {
@@ -27,6 +94,7 @@ function addContactFromModal() {
         document.getElementById("new-contact-modal-key").value
     );
 }
+
 function addContact(name, email, key) {
     return localforage.getItem('guzi-contacts').then(contacts => {
         if (contacts === null) { contacts = [] }
